4568c29f8bd51408a377ec6818cd762ee97ea6ee
Lindsay Parker <thekillustrator@gmail.com>
2020-08-17 20:44:28 +0000

    add sort to reducer

--- src/app/reducers/todos.reducer.ts
+++ src/app/reducers/todos.reducer.ts
@@ -1,5 +1,6 @@
 import { EntityState, createEntityAdapter } from '@ngrx/entity';
-import { createReducer, Action } from '@ngrx/store';
+import { createReducer, Action, on } from '@ngrx/store';
+import * as actions from '../actions/todo.actions';
 
 export interface TodoEntity {
   id: string;
@@ -10,7 +11,7 @@ export interface TodoEntity {
 }
 
 export interface TodoState extends EntityState<TodoEntity> {
-
+  sort: string[];
 }
 
 export const adapter = createEntityAdapter<TodoEntity>();
@@ -22,13 +23,36 @@ const initialState: TodoState = {
     1: { id: '1', name: 'Change light bulbs', project: 'Home', completed: false },
     2: { id: '2', name: 'Clean garage', completed: true },
     3: { id: '3', name: 'Take car to shop', completed: false }
-  }
+  },
+  sort: ['1', '2', '3']
 };
 
 const reducerFunction = createReducer(
-  initialState
+  initialState,
+  on(actions.todoItemSorted, (state, action) => {
+    const newSort = move(state.sort, action.previousIndex, action.currentIndex);
+    return { ...state, sort: newSort };
+  })
 );
 
 export function reducer(state: TodoState = initialState, action: Action): TodoState {
   return reducerFunction(state, action);
+}
+
+function move<T>(arr: T[], oldIndex: number, newIndex: number): T[] {
+  arr = [...arr];
+  while (oldIndex < 0) {
+    oldIndex += arr.length;
+  }
+  while (newIndex < 0) {
+    newIndex += arr.length;
+  }
+  if (newIndex >= arr.length) {
+    let k = newIndex - arr.length;
+    while ((k--) + 1) {
+      arr.push(undefined);
+    }
+  }
+  arr.splice(newIndex, 0, arr.splice(oldIndex, 1)[0]);
+  return arr;
 }
