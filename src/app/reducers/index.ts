70cbe2134bfb89f59facf0b288109417332ed264
Lindsay Parker <thekillustrator@gmail.com>
2020-08-17 19:59:54 +0000

    Fill out todos reducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,10 +1,13 @@
 import { ActionReducerMap } from '@ngrx/store/';
 import * as fromProjects from './projects.reducer';
+import * as fromTodos from './todos.reducer';
 
 export interface AppState {
   projects: fromProjects.ProjectState;
+  todos: fromTodos.TodoState;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-  projects: fromProjects.reducer
+  projects: fromProjects.reducer,
+  todos: fromTodos.reducer
 };
---
+++ src/app/reducers/todos.reducer.ts
@@ -0,0 +1,34 @@
+import { EntityState, createEntityAdapter } from '@ngrx/entity';
+import { createReducer, Action } from '@ngrx/store';
+
+export interface TodoEntity {
+  id: string;
+  name: string;
+  project?: string;
+  dueDate?: string;
+  completed: boolean;
+}
+
+export interface TodoState extends EntityState<TodoEntity> {
+
+}
+
+export const adapter = createEntityAdapter<TodoEntity>();
+
+// const initialState = adapter.getInitialState();
+const initialState: TodoState = {
+  ids: ['1', '2', '3'],
+  entities: {
+    1: { id: '1', name: 'Change light bulbs', project: 'Home', completed: false },
+    2: { id: '2', name: 'Clean garage', project: 'Home', completed: true },
+    3: { id: '3', name: 'Take car to shop', project: 'Home', completed: false }
+  }
+};
+
+const reducerFunction = createReducer(
+  initialState
+);
+
+export function reducer(state: TodoState = initialState, action: Action): TodoState {
+  return reducerFunction(state, action);
+}
