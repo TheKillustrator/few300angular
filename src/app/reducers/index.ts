fa7410dda31d26a5f674a83bed845c06f970fd99
Lindsay Parker <thekillustrator@gmail.com>
2020-08-17 20:06:38 +0000

    add selectors

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,6 +1,7 @@
-import { ActionReducerMap } from '@ngrx/store/';
+import { ActionReducerMap, createSelector } from '@ngrx/store/';
 import * as fromProjects from './projects.reducer';
 import * as fromTodos from './todos.reducer';
+import * as fromModels from '../models';
 
 export interface AppState {
   projects: fromProjects.ProjectState;
@@ -11,3 +12,17 @@ export const reducers: ActionReducerMap<AppState> = {
   projects: fromProjects.reducer,
   todos: fromTodos.reducer
 };
+
+// Selectors
+// One per "branch" of the state.
+const selectProjectBranch = (state: AppState) => state.projects;
+const selectTodosBranch = (state: AppState) => state.todos;
+
+// Helpers
+const { selectAll: selectAllTodos } = fromTodos.adapter.getSelectors(selectTodosBranch);
+
+// Selectors for Components
+export const selectAllTodoList = createSelector(
+  selectAllTodos,
+  (todos) => todos as fromModels.TodoItem[]
+);
